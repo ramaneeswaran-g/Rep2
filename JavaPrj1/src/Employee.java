d7f5f528970ca843d460c81a249be2092cdb32e4
Easwar <ramaneeswaran-g@gmail.com>
2021-03-06 14:46:15 +0530

    Commit1

---
+++ JavaPrj1/src/Employee.java
@@ -0,0 +1,20 @@
+
+public class Employee {
+   private int Eno;
+   private String Ename;
+   private String Edesg;
+   private String Edept;
+   
+   public Employee()
+   {
+	   Eno=101;
+	   Ename=new String("Sasi");
+	   Edesg=new String("Dev");
+	   Edept=new String("Prod");
+   }
+   public void dispEmp()
+   {
+	   System.out.print("Eno =" + Eno);
+	   
+   }
+}
